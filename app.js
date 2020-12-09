2cf2de75c53f1fd08531af7b12a42658c06cbd52
Noah Heath <noahadamheath@gmail.com>
2020-12-09 14:30:00 -0500

    Properly requests metadata

--- app.js
+++ app.js
@@ -9,11 +9,33 @@ var ratings = reviews.collection('ratings');
 
 app.use(bodyParser.json());
 
-app.get('/reviews/:id', (req, res) => {
+app.get('/reviews(/meta)?/:id', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
-    console.log(req.params);
     ratings.findOne({id: Number(req.params.id)}).then((data) => {
-        res.status(200).send(data);
+        if (req.params['0'] === undefined) {
+            res.status(200).send(data);
+        } else {
+            var meta = {
+                'product_id': data.id,
+                'ratings' : {1: 0, 2: 0, 3: 0, 4: 0, 5: 0},
+                'recommended' : 0,
+                'characteristics' : {}};
+            var chars = {};
+            for (var item of data.characteristics) {
+                meta['characteristics'][item] = 0;
+                chars[item] = 0;
+            }
+            for (var review of data.reviews) {
+                meta['ratings'][review.rating] ++;
+                meta['recommended'] += review.recommend ? 1 : 0;
+                for (var char of review.characteristics) {
+                    var total = chars[char.name] * meta['characteristics'][char.name];
+                    chars[char.name]++;
+                    meta['characteristics'][char.name] = Number(((total + char.value) / chars[char.name]).toFixed(2));
+                }
+            }
+            res.status(200).send(meta);
+        }
     });
 });
 app.post('/reviews/:id', (req, res) => {
