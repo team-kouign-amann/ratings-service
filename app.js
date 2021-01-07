ce9ea154ab816791ce201bc77c2615895fe8a3b5
Noah Heath <68669661+psychout99@users.noreply.github.com>
2021-01-06 22:45:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var mongoose = require("mongoose");
-//psychout98:M%40rt33ny@3.15.194.153:27017
-var conn = mongoose.createConnection("mongodb://psychout98:M%40rt33ny@3.15.194.153:27017",
+var conn = mongoose.createConnection("mongodb://user:pass@3.15.194.153:27017",
     {useNewUrlParser: true, useUnifiedTopology: true});
 var reviews = conn.useDb('reviews');
 var ratings = reviews.collection('products');
